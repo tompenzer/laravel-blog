285cc63d9cce814b713f032ec921fce88a0fd1ad
Tom Penzer <tpenzer@mailcan.com>
2018-06-04 17:55:52 -0700

    Make medialibrary use a newly added AWS_PUBLIC_ENDPOINT environmental variable, so we can easily link to a region-specific s3 bucket and bypass the s3 redirect.

--- config/medialibrary.php
+++ config/medialibrary.php
@@ -29,7 +29,7 @@ return [
         /*
          * The domain that should be prepended when generating urls.
          */
-        'domain' => 'https://' . env('AWS_BUCKET') . '.s3.amazonaws.com',
+        'domain' => env('AWS_PUBLIC_ENDPOINT'),
     ],
 
     'remote' => [
