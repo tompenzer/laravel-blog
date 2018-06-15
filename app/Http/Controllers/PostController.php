86dfed6b31de41b7fee17fc0718f50d16875b424
Tom Penzer <tpenzer@mailcan.com>
2018-06-14 20:00:53 -0700

    Put a 10 minute cache on the public posts content.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,23 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use Cache;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class PostController extends Controller
 {
+    const CACHE_DURATION_POSTS_MIN = 10;
+
     /**
      * Show the application dashboard.
      */
     public function index(Request $request): View
     {
-        return view('posts.index', [
-            'posts' => Post::search($request->input('q'))
+        $page = 1;
+        $search = null;
+
+        if ($request->has('page')) {
+            $page = $request->input('page');
+        }
+
+        if ($request->has('q')) {
+            $search = $request->input('q');
+        }
+
+        $cache_name = "posts:search:{$search}:page:{$page}";
+
+        if (Cache::has($cache_name)) {
+            $posts = Cache::get($cache_name);
+        } else {
+            $posts = Post::search($search)
                              ->with('author')
                              ->withCount('thumbnail')
                              ->latest()
-                             ->paginate(20)
+                             ->paginate(20);
+
+            Cache::put($cache_name, $posts, self::CACHE_DURATION_POSTS_MIN);
+        }
+
+        return view('posts.index', [
+            'posts' => $posts
         ]);
     }
 
