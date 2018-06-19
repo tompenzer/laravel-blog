615737d9c37e30a35d00ede8fce2241731a8ab5d
Tom Penzer <tpenzer@mailcan.com>
2018-06-19 13:32:23 -0700

    Fix comment description of Locale middleware class.

--- app/Http/Middleware/Locale.php
+++ app/Http/Middleware/Locale.php
@@ -7,13 +7,16 @@ use Config;
 use Illuminate\Http\Request;
 
 /**
- * Syncs 'locale' session data to requested language, sets app locale to session
- * locale.
+ * Sets app locale to user-agent-requested locale if valid.
  */
 class Locale
 {
     public function handle(Request $request, Closure $next) {
-        app()->setLocale($request->getPreferredLanguage(Config::get('app.locales_supported')));
+        app()->setLocale(
+            $request->getPreferredLanguage(
+                Config::get('app.locales_supported')
+            )
+        );
 
         return $next($request);
     }
